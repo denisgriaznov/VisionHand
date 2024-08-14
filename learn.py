d2d7e394330ce4bed70fcbb12f9143b5df2876b6
dengriaznov <dengriaznov@gmail.com>
2024-08-14 20:37:54 +0600

    Arg parser was added

--- learn.py
+++ learn.py
@@ -6,12 +6,6 @@ import argparse
 def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--path",
-        type=str,
-        help="Path towards model xml file",
-        required=True
-    )
-    parser.add_argument(
         "--obs_mode",
         type=str,
         help="Mode of observation",
@@ -24,9 +18,8 @@ def create_parser():
 if __name__ == "__main__":
     parser = create_parser()
     args = parser.parse_args()
-    path = args.path
-    obs_mode = args.obs_mode
-    env = VisionHandGraspEnv(obs_mode='obs_mode')
+Ar    obs_mode = args.obs_mode
+    env = VisionHandGraspEnv(obs_mode=obs_mode)
 
     model = PPO("MlpPolicy", env, verbose=1)
     model.learn(total_timesteps=25000)
